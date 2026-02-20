3b86e09855a11ce8c9df5309ad789e2660494d14
RaeII <israel.zeferino@hotmail.com>
2026-02-20 11:17:05 -0300

    feat: Nativewind

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,6 +1,7 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Stack } from "expo-router";
 import { SafeAreaView } from "react-native-safe-area-context";
+import "@/styles/global.css";
 
 const queryClient = new QueryClient();
 
